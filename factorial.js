fcd7c3aa6d62314cfd1d8758c92b13c99a3d6068
Rahma-silliman <119931759+Rahma-silliman@users.noreply.github.com>
2023-02-17 23:43:43 +0100

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -1,5 +1,5 @@
 // Écrire une fonction qui calcule le factoriel d'un nombre (le produit de tous les nombres entiers de 1 à ce nombre). • 1 < n   
-function factoriel(n){
+function factorial(n){
   let calculFactoriel = 1;
   for(let i = 1; i <= n; i++){
    calculFactoriel *= i;
